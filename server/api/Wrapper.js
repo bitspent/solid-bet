475a39666c2473bead07005219ab374f1ad7cbb5
hassanjawhar <devhassanjawhar@gmail.com>
2018-11-04 22:41:19 +0200

    Updated

--- server/api/Wrapper.js
+++ server/api/Wrapper.js
@@ -1,120 +1,147 @@
-let footballdata = new (require('../api/bets/sports/FootballData'));
-let Web3 = require('web3');
+let footballdata = new (require("../api/bets/sports/FootballData"))();
+let Web3 = require("web3");
 
 class Wrapper {
-    constructor(network, db) {
-        this.web3 = new Web3(new Web3.providers.WebsocketProvider(`wss://${network}.infura.io/ws`));
-        this.updating_contracts = false;
-        this.db = db;
-    }
+  constructor(network, db) {
+    this.web3 = new Web3(
+      new Web3.providers.WebsocketProvider(`wss://${network}.infura.io/ws`)
+    );
+    this.updating_contracts = false;
+    this.db = db;
+    this.network = network;
+  }
 
-    async getData(_data, _start, _end) {
-        if (_start === _end) {
-            console.log("Successfully got all pending contracts receipts.");
-            this.updating_contracts = false;
-        } else {
-            this.updating_contracts = true;
-            let contract = _data[_start];
-            let receipt = await this.web3.eth.getTransactionReceipt(contract["transactionHash"]);
-            if (receipt) {
-                if (receipt['status'] === true) {
-                    let updated = await this.db.updateData('contracts', {
-                            transactionHash: receipt['transactionHash']
-                        },
-                        {
-                            "contractAddress": receipt["contractAddress"],
-                            "from": receipt["from"],
-                            "status": receipt['status']
-                        });
-                }
+  async getData(_data, _start, _end) {
+    if (_start === _end) {
+      console.log("Successfully got all pending contracts receipts.");
+      this.updating_contracts = false;
+    } else {
+      this.updating_contracts = true;
+      let contract = _data[_start];
+      let receipt = await this.web3.eth.getTransactionReceipt(
+        contract["transactionHash"]
+      );
+      if (receipt) {
+        if (receipt["status"] === true) {
+          let updated = await this.db.updateData(
+            "contracts",
+            {
+              transactionHash: receipt["transactionHash"]
+            },
+            {
+              contractAddress: receipt["contractAddress"],
+              from: receipt["from"],
+              status: receipt["status"]
             }
-            _start++;
-            this.getData(_data, _start, _end);
+          );
         }
+      }
+      _start++;
+      this.getData(_data, _start, _end);
     }
+  }
 
-    updateContracts() {
-        if (this.updating_contracts === false) {
-            this.db.viewData('contracts', {
-                contractAddress: null
-            }, {
-                id: true,
-                matchId: true,
-                transactionHash: true,
-                to: true,
-                from: true,
-                time: true
-            }).then(data => {
-                if (data.length === 0) {
-                    console.log("There's nothing to update.");
-                } else {
-                    console.log(`Updating ${data.length} contracts.`);
-                    this.getData(data, 0, data.length);
-                }
-            }).catch(err => {
-                console.log(err);
-            });
-        } else {
-
-        }
-    }
-
-    async insertMatches(league) {
-        let data = await footballdata.getLeagueMatches(league);
-        let matches = data.matches;
-        let competition = data['competition'];
-        let keyyed_matches = {};
-
-        matches.forEach(match => {
-            keyyed_matches[match.id] = match;
-            keyyed_matches[match.id]["league"] = competition["name"];
-            // console.log(match)
-            this.db.insertData('matches', match)
-                .then(result => {
-                    // console.log(`Successfully inserted matches`)
-                })
-                .catch(err => {
-                    console.log(`Failed to insert matches.`);
-                });
+  updateContracts() {
+    if (this.updating_contracts === false) {
+      this.db
+        .viewData(
+          "contracts",
+          {
+            contractAddress: null
+          },
+          {
+            id: true,
+            matchId: true,
+            transactionHash: true,
+            to: true,
+            from: true,
+            time: true
+          }
+        )
+        .then(data => {
+          if (data.length === 0) {
+            console.log("There's nothing to update.");
+          } else {
+            console.log(`Updating ${data.length} contracts.`);
+            this.getData(data, 0, data.length);
+          }
+        })
+        .catch(err => {
+          console.log(err);
         });
-        console.log(`League: ${league} - successfully inserted ${matches.length} matches`)
+    } else {
     }
+  }
 
-    async updateData(league) {
-        let data = await footballdata.getLeagueMatches(league);
-        let matches = data.matches;
-        let competition = data['competition'];
-        let keyyed_matches = {};
-        let error = false;
-        matches.forEach(match => {
-            keyyed_matches[match.id] = match;
-            keyyed_matches[match.id]["league"] = competition["name"];
+  async insertMatches(league) {
+    let data = await footballdata.getLeagueMatches(league);
+    let matches = data.matches;
+    let competition = data["competition"];
+    let keyyed_matches = {};
 
-            this.db.updateData('matches',
-                {
-                    data: {
-                        matchId: [match.id],
-                    }
-                }, {
-                    data: match
-                }
-            )
-                .then(result => {
-                    // console.log(`Successfully updated match ${match.id}`)
-                })
-                .catch(err => {
-                    console.log(`Failed to update match ${match.id}`);
-                });
+    matches.forEach(match => {
+      keyyed_matches[match.id] = match;
+      keyyed_matches[match.id]["league"] = competition["name"];
+      // console.log(match)
+      this.db
+        .insertData("matches", match)
+        .then(result => {
+          // console.log(`Successfully inserted matches`)
+        })
+        .catch(err => {
+          console.log(`Failed to insert matches.`);
         });
-    }
+    });
+    console.log(
+      `League: ${league} - successfully inserted ${matches.length} matches`
+    );
+  }
+
+  async updateData(league) {
+    let data = await footballdata.getLeagueMatches(league);
+    let matches = data.matches;
+    let competition = data["competition"];
+    let keyyed_matches = {};
+    let error = false;
+    matches.forEach(match => {
+      keyyed_matches[match.id] = match;
+      keyyed_matches[match.id]["league"] = competition["name"];
+
+      this.db
+        .updateData(
+          "matches",
+          {
+            data: {
+              matchId: [match.id]
+            }
+          },
+          {
+            data: match
+          }
+        )
+        .then(result => {
+          // console.log(`Successfully updated match ${match.id}`)
+        })
+        .catch(err => {
+          console.log(`Failed to update match ${match.id}`);
+        });
+    });
+  }
+
+  renewWeb3Instance() {
+    this.web3 = new Web3(
+      new Web3.providers.WebsocketProvider(`wss://${this.network}.infura.io/ws`)
+    );
+    console.log("Successfully renewed Web3 instance.");
+  }
 }
 
 function sleep(seconds) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            return resolve(true);
-        }, seconds * 1000);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return resolve(true);
+    }, seconds * 1000);
+  });
 }
 
 module.exports = Wrapper;
--- server/app.js
+++ server/app.js
@@ -1,86 +1,121 @@
-r = require('rethinkdb');
-db = new (require('./api/rethinkdb/Database'));
-var cors = require('cors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-let wrapper = new (require('./api/Wrapper'))('ropsten', db);
+r = require("rethinkdb");
+db = new (require("./api/rethinkdb/Database"))();
+var cors = require("cors");
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+let wrapper = new (require("./api/Wrapper"))("ropsten", db);
 var app = express();
 app.use(cors());
-let insert_matches = true;
+let insert_matches = false;
 
 db.initializeConnection()
-    .then(async conn => {
-        r.connection = conn;
-        console.log("Successfully connected to rethinkdb server");
-        if (insert_matches) {
-            wrapper.insertMatches('CL')
-                .then(result => {
-                })
-                .catch(err => {
-                    console.log(err)
-                });
-        }
-    })
-    .catch(err => {
-        console.log(err)
-        console.log("Failed to connect to rethinkdb server");
-    });
+  .then(async conn => {
+    r.connection = conn;
+    console.log("Successfully connected to rethinkdb server");
+    if (insert_matches) {
+      wrapper
+        .insertMatches("CL")
+        .then(result => {})
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    console.log("Failed to connect to rethinkdb server");
+  });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 /**
  * Routes
  */
-app.use('/', require('./routes/index'));
-app.use('/matches', require('./routes/matches'));
-app.use('/crypto', require('./routes/crypto'));
-app.use('/contracts', require('./routes/contracts'));
+app.use("/", require("./routes/index"));
+app.use("/matches", require("./routes/matches"));
+app.use("/crypto", require("./routes/crypto"));
+app.use("/contracts", require("./routes/contracts"));
 // app.use('/bets', require('./routes/bets'));
 
 /**
  * API endpoints
  */
-app.use('/v1/matches', require('./api/endpoints/Matches'));
-app.use('/v1/crypto', require('./api/endpoints/Crypto'));
+app.use("/v1/matches", require("./api/endpoints/Matches"));
+app.use("/v1/crypto", require("./api/endpoints/Crypto"));
 
-app.post('/v1/contracts/add', require('./api/endpoints/Contracts').insertContract);
-app.post('/v1/contracts/fetch', require('./api/endpoints/Contracts').showContract);
-app.post('/v1/contracts/mine', require('./api/endpoints/Contracts').showMyContracts);
-app.post('/v1/contracts/public', require('./api/endpoints/Contracts').showPublicContracts);
-app.post('/v1/contracts/private', require('./api/endpoints/Contracts').showPrivateContracts);
-app.post('/v1/contracts/owned', require('./api/endpoints/Contracts').showOwnedContracts);
-app.post('/v1/contracts/length', require('./api/endpoints/Contracts').showContractsLength);
+app.post(
+  "/v1/contracts/add",
+  require("./api/endpoints/Contracts").insertContract
+);
+app.post(
+  "/v1/contracts/fetch",
+  require("./api/endpoints/Contracts").showContract
+);
+app.post(
+  "/v1/contracts/mine",
+  require("./api/endpoints/Contracts").showMyContracts
+);
+app.post(
+  "/v1/contracts/public",
+  require("./api/endpoints/Contracts").showPublicContracts
+);
+app.post(
+  "/v1/contracts/private",
+  require("./api/endpoints/Contracts").showPrivateContracts
+);
+app.post(
+  "/v1/contracts/owned",
+  require("./api/endpoints/Contracts").showOwnedContracts
+);
+app.post(
+  "/v1/contracts/length",
+  require("./api/endpoints/Contracts").showContractsLength
+);
 
-app.post('/v1/bets/add', require('./api/endpoints/Bets').insertBet);
+app.post("/v1/bets/add", require("./api/endpoints/Bets").insertBet);
 
-app.post('/v1/contracts/inactive/all', require('./api/endpoints/Contracts').showInactiveContracts);
-app.post('/v1/bets/inactive/all', require('./api/endpoints/Bets').showInactiveBets);
+app.post(
+  "/v1/contracts/inactive/all",
+  require("./api/endpoints/Contracts").showInactiveContracts
+);
+app.post(
+  "/v1/bets/inactive/all",
+  require("./api/endpoints/Bets").showInactiveBets
+);
 
-app.post('/v1/contracts/inactive/mine', require('./api/endpoints/Contracts').showOwnedInactiveContracts);
-app.post('/v1/bets/inactive/mine', require('./api/endpoints/Bets').showOwnedInactiveBets);
+app.post(
+  "/v1/contracts/inactive/mine",
+  require("./api/endpoints/Contracts").showOwnedInactiveContracts
+);
+app.post(
+  "/v1/bets/inactive/mine",
+  require("./api/endpoints/Bets").showOwnedInactiveBets
+);
 
 // app.post('/v1/bets/fetch', require('./api/endpoints/Bets').showActiveBets);
 // app.post('/v1/bets/fetch', require('./api/endpoints/Bets').showBet);
 
 setTimeout(() => {
-    wrapper.updateContracts();
+  wrapper.updateContracts();
 }, 2500);
 
 setInterval(() => {
-    wrapper.updateContracts();
+  wrapper.updateContracts();
 }, 10 * 1000);
 
 // setTimeout(async () => {
@@ -89,26 +124,30 @@ setInterval(() => {
 // }, 10 * 1000);
 //
 setTimeout(async () => {
-    let updateData = await wrapper.updateData('CL');
-    console.log("Successfully updated all matches.");
+  let updateData = await wrapper.updateData("CL");
+  console.log("Successfully updated all matches.");
 }, 30 * 60 * 1000);
 
+setInterval(() => {
+  wrapper.renewWeb3Instance();
+}, 5 * 60 * 1000);
+
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
